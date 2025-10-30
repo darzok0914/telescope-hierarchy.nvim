03f971f08d06bdd073c4c202bb8dfd78dacf23d9
Benjamin P.(T0307316) <benjamin.papin@thalesgroup.com>
2025-10-30 22:05:31 +0100

    feat: lexicographically sort incoming and outcoming calls

--- lua/telescope-hierarchy/tree/node.lua
+++ lua/telescope-hierarchy/tree/node.lua
@@ -251,7 +251,7 @@ function Node:search(callback)
               ref.uri,
               outter_func.function_name,
               ref.range.start.line + 1,
-              ref.range.start.character + 1,
+              ref.range.start.character, -- we don't do +1 to prevent telescope to shift by one when we jump to the location
               entry
             )
           end
@@ -262,7 +262,13 @@ function Node:search(callback)
         -- Check for duplicate ranges from LSP
         -- Assumes the duplicates are sequential. Would need to do more work if they are unordered
         if range.start.line ~= last_line or range.start.character ~= last_char then
-          self:new_child(uri, inner.name, range.start.line + 1, range.start.character + 1, entry)
+          self:new_child(
+            uri,
+            inner.name,
+            range.start.line + 1,
+            range.start.character, -- we don't do +1 to prevent telescope to shift by one when we jump to the location
+            entry
+          )
           last_line = range.start.line
           last_char = range.start.character
         end
@@ -429,9 +435,18 @@ end
 
 ---@param arr Node[] The array of nodes to sort
 ---@return Node[] The sorted array
-local function quicksort_nodes(arr)
+local function quicksort_nodes(filepath_active_buffer, arr)
   if #arr <= 1 then
     return arr
+  end
+
+  ---@param node Node The node to encode
+  local encode_node = function(node)
+    if node.filename == filepath_active_buffer then
+      return node.filename .. string.format("%05d", node.lnum)
+    else
+      return Path:new(node.filename):normalize(vim.uv.cwd()) .. string.format("%05d", node.lnum)
+    end
   end
 
   local pivot = arr[1]
@@ -439,15 +454,15 @@ local function quicksort_nodes(arr)
   local right = {}
 
   for i = 2, #arr do
-    if arr[i].lnum < pivot.lnum then
+    if encode_node(arr[i]) < encode_node(pivot) then
       table.insert(left, arr[i])
     else
       table.insert(right, arr[i])
     end
   end
 
-  local sorted_left = quicksort_nodes(left)
-  local sorted_right = quicksort_nodes(right)
+  local sorted_left = quicksort_nodes(filepath_active_buffer, left)
+  local sorted_right = quicksort_nodes(filepath_active_buffer, right)
 
   return vim.iter({ sorted_left, { pivot }, sorted_right }):flatten():totable()
 end
@@ -461,22 +476,19 @@ end
 ---@param list NodeList The list being built up
 ---@param node Node
 ---@param tree_state boolean[] A list of true/false flags that, for each level in indicate whether this is the last node. This information is needed for rendering the tree in the Telescope finder
-local function add_node_to_list(list, node, tree_state)
+local function add_node_to_list(filepath_active_buffer, list, node, tree_state)
   local entry = {
     node = node,
     tree_state = tree_state,
   }
   table.insert(list, entry)
   if node.expanded and #node.children > 0 then
-    local direction = assert(state.direction())
-    if not direction:is_incoming() then
-      node.children = quicksort_nodes(node.children)
-    end
+    node.children = quicksort_nodes(filepath_active_buffer, node.children)
     for idx, child in ipairs(node.children) do
       local last_child = idx == #node.children
       local new_state = { unpack(tree_state) }
       table.insert(new_state, last_child)
-      add_node_to_list(list, child, new_state)
+      add_node_to_list(filepath_active_buffer, list, child, new_state)
     end
   end
 end
@@ -490,7 +502,7 @@ function Node:to_list(from_root)
   ---@type NodeList
   local results = {}
   local render_root = (from_root == nil or from_root) and self.root or self
-  add_node_to_list(results, render_root, {})
+  add_node_to_list(self.root.filename, results, render_root, {})
   return results
 end
 
